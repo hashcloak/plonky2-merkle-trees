9bd8eb72050e2353a87747a8512e0d9958e4f900
Manish Kumar <kumarmanish19963@gmail.com>
2023-07-30 00:11:48 +0530

    insert, witness & verify added. verification giving false

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use std::iter;
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct IncrementalTree<F: RichField, H: Hasher<F>> {
     root: H::Hash,
-    zeros: Vec<H::Hash>,
+    zeroes: Vec<H::Hash>,
     nodes: Vec<Vec<H::Hash>>,
     depth: usize,
     position: usize,
@@ -40,7 +40,96 @@ impl<F: RichField, H: Hasher<F>> IncrementalTree<F, H> {
 
         assert_eq!(zeroes.len(), depth + 1);
 
-        IncrementalTree { root: *zeroes.last().unwrap(), zeros: zeroes, nodes: vec![Vec::new(); depth], depth: depth, position: 0 }
+        IncrementalTree { root: *zeroes.last().unwrap(), zeroes: zeroes, nodes: vec![Vec::new(); depth], depth: depth, position: 0 }
+    }
+
+    pub fn insert(&mut self, leaf: H::Hash) {
+
+        if leaf == self.zeroes[0] {
+            panic!("leaf cannot be zero");
+        }
+
+        if self.nodes.len() >= usize::pow(2, self.depth.try_into().unwrap()) {
+            panic!("Tree is full");
+        }
+
+        let IncrementalTree {root, zeroes, nodes, depth, position} = self;
+
+        let mut append_leaf = |node, level, index| {
+            let level = level as usize;
+
+            if nodes[level].len() > index { 
+                nodes[level][index] = node; 
+            } else {
+                nodes[level].push(node);
+            }
+
+            if (index % 2) == 1 {
+                H::two_to_one(nodes[level][index - 1], node)
+            } else {
+                H::two_to_one(node, zeroes[level])
+            }
+
+        };
+
+        let mut node = leaf;
+        let mut index = *position;
+
+        for level in 0..*depth {
+            node = append_leaf(node, level, index);
+            index = (index as f64 / 2 as f64).floor() as usize;
+        }
+
+        *position += 1;
+        *root = node;
+
+        ()
+
+    }
+
+    pub fn witness(&mut self, leaf: H::Hash) -> (Vec<H::Hash>, Vec<bool>) {
+        let IncrementalTree {zeroes, nodes, depth, .. } = self;
+
+        let index = nodes[0].iter().position(|&el| el == leaf);
+        // println!("{:?}", index);
+        if index.is_none() {
+            panic!("no such leaf");
+        }
+
+        let mut index = index.unwrap();
+        let mut siblings = vec![zeroes[0];depth.clone()];
+        let mut pos = vec![false; depth.clone()];
+
+        let mut sibling_path = |level, index| {
+            let level = level as usize;
+
+            if index % 2 == 1 {
+                siblings[level] = nodes[level][index - 1];
+                pos[level] = true;
+            } else {
+                siblings[level] = zeroes[level];
+            }
+        };
+
+        for level in 0..*depth {
+            sibling_path(level, index);
+            index = (index as f64 / 2 as f64).floor() as usize;
+        }
+
+        (siblings, pos)
+    }
+
+    pub fn check_proof(&self, leaf: H::Hash, siblings: Vec<H::Hash>, pos: Vec<bool>) -> bool {
+        let mut node = leaf;
+        for (sibling, p) in siblings.iter().zip(pos.iter()) {
+            if *p {
+                node = H::two_to_one(node, *sibling);
+            } else {
+                node = H::two_to_one(*sibling, node);
+            }
+        }
+
+        node == self.root
     }
 }
 
@@ -62,8 +151,34 @@ fn main() {
     let cap_height = 3;
     // let leaves = random_data::<F>(n, 7);
     let zero = vec![GoldilocksField::from_canonical_u64(0)];
+    let one = vec![GoldilocksField::from_canonical_u64(1)];
+    let two = vec![GoldilocksField::from_canonical_u64(2)];
+    let three = vec![GoldilocksField::from_canonical_u64(3)];
+    let four = vec![GoldilocksField::from_canonical_u64(4)];
+    let five = vec![GoldilocksField::from_canonical_u64(5)];
+
+
+
     let zero_hash = PoseidonHash::hash_or_noop(&zero);
-    let tree = IncrementalTree::<F, <C as GenericConfig<D>>::Hasher>::new(zero_hash, cap_height);
+    let one_hash = PoseidonHash::hash_or_noop(&one);
+    let two_hash = PoseidonHash::hash_or_noop(&two);
+    let three_hash = PoseidonHash::hash_or_noop(&three);
+    let four_hash = PoseidonHash::hash_or_noop(&four);
+    let five_hash = PoseidonHash::hash_or_noop(&five);
+
+
+    let mut tree = IncrementalTree::<F, <C as GenericConfig<D>>::Hasher>::new(zero_hash, cap_height);
     println!("{:?}", tree);
+
+    tree.insert(one_hash);
+    tree.insert(two_hash);
+    tree.insert(three_hash);
+    tree.insert(four_hash);
+    tree.insert(five_hash);
+
+    println!("{:?}", tree);
+
+    let (siblings, pos) = tree.witness(five_hash);
+    println!("{:?}", tree.check_proof(five_hash, siblings, pos));
     println!("Hello, world!");
 }
